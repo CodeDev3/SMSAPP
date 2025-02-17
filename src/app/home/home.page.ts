f96078a8e375c872b0bf90363f2ce67cb6000287
CodeDev3 <jquerydeveloper3@gmail.com>
2025-02-17 22:34:38 +0530

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,34 @@ export class HomePage {
 
   constructor() {}
 
+readFile(event: any) {
+    console.log(event);
+    const file = event.target.files[0];
+    const fileType = file.type.split('/')[0];
+    console.log('File type:', fileType);
+    console.log('File uploaded:', file);
+    const fileDisplay = document.getElementById('file-display');
+    if (fileDisplay) {
+      fileDisplay.innerHTML = '';
+    }
+    const reader = new FileReader();
+    if (fileType === 'image') {
+      const imageUrl = URL.createObjectURL(file);
+      const imgElement = document.createElement('img');
+      imgElement.src = imageUrl;
+      imgElement.alt = 'Uploaded Image';
+      imgElement.style.minWidth = '100%';
+      imgElement.style.minHeight = '400px';
+      document.getElementById('file-display')?.appendChild(imgElement);
+    } else if (fileType === 'video') {
+      const videoUrl = URL.createObjectURL(file);
+      const videoElement = document.createElement('video');
+      videoElement.src = videoUrl;
+      videoElement.controls = true;
+      videoElement.style.maxWidth = '100%';
+      videoElement.style.maxHeight = '400px';
+      document.getElementById('file-display')?.appendChild(videoElement);
+    }
+    event.target.value = '';
+  }
 }
